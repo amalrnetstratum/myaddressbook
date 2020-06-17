714ba20f7f54a79c8eca23dc9a15ebd9f8c70407
amalrnetstratum <amalr@netstratum.com>
2020-06-17 08:00:01 +0000

    2020-06-17 Update
    Tags like Favorites and Blocked can be added to an entry as follows
    url/tag/uuid|name/<tagvalue>
    Also records with tags can be viewed as follows
    url/view/tag/<tagvalue>

--- src/myaddressbook_api.erl
+++ src/myaddressbook_api.erl
@@ -16,30 +16,51 @@ initialize() ->
   bifrost_api:initialize(?MODULE).
 
 init([]) ->
-  Routes = [#{path =>	"/insert/[:name]/[:number]",
-              auth => false,
-			        functions => #{get	=> {myaddressbook_client, insert_address_api},
-							put => {myaddressbook_client, insert_address_api},
-							options => {myaddressbook_api, handle_options}}},
-			#{path =>	"/view/[:name]",
-        auth => false,
-			functions => #{get	=> {myaddressbook_client, view_address_api},
-							put => {myaddressbook_client, view_address_api},
-							options => {myaddressbook_api, handle_options}}}
-           ],
+  Routes = 
+    [#{path => "/insert/[:name]/[:number]",
+      auth => false,
+	  functions => #{get => {myaddressbook_client, insert_address_api},
+					put => {myaddressbook_client, insert_address_api},
+					options => {myaddressbook_api, handle_options}}}
+	,#{path => "/view/name/[:name]",
+      auth => false,
+	  functions => #{get => {myaddressbook_client, view_address_api},
+	  			    put => {myaddressbook_client, view_address_api},
+	  			    options => {myaddressbook_api, handle_options}}}
+	,#{path => "/view/uuid/[:uuid]",
+      auth => false,
+	  functions => #{get => {myaddressbook_client, view_address_api},
+	  			    put => {myaddressbook_client, view_address_api},
+	  			    options => {myaddressbook_api, handle_options}}}
+    ,#{path => "/view/tag/[:tags]",
+      auth => false,
+	  functions => #{get => {myaddressbook_client, viewtag_address_api},
+					put => {myaddressbook_client, viewtag_address_api},
+					options => {myaddressbook_api, handle_options}}}
+    ,#{path => "/tag/uuid/[:uuid]/[:tag]",
+      auth => false,
+	  functions => #{get => {myaddressbook_client, tag_address_api},
+					put => {myaddressbook_client, tag_address_api},
+					options => {myaddressbook_api, handle_options}}}
+	,#{path => "/tag/name/[:name]/[:tag]",
+      auth => false,
+      functions => #{get => {myaddressbook_client, tag_address_api},
+					put => {myaddressbook_client, tag_address_api},
+					options => {myaddressbook_api, handle_options}}}
+    ],
   {ok, Routes}.
 
 handle_options(Headers, ReqParams, _PathInfo, _State) ->
-	lager:info("Got the headers for options:~p",[Headers]),
-	lager:info("Got the request for the options:~p",[ReqParams]),
-	ACRequestHeaders = maps:get(<<"access-control-request-headers">>, Headers, <<"*">>),
-	RequestMethod = maps:get(<<"access-control-request-method">>, Headers),
-	lager:info("ACRequestHeaders:~p~n",[ACRequestHeaders]),
-	ResponseHeaders =  
-		#{<<"content-type">>				=>	<<"application/json;charset=utf-8">>,
-		<<"Access-Control-Allow-Headers">>	=>	ACRequestHeaders,
-		<<"Access-Control-Allow-Methods">>	=>	<<RequestMethod/binary, ",OPTIONS">>,
-		<<"Access-Control-Max-Age">>		=>	<<"1728000">>},
-	lager:info("ResponseHeaders for options:~p:",[ResponseHeaders]),
-	{204, [], ResponseHeaders}.
+  lager:info("Got the headers for options:~p",[Headers]),
+  lager:info("Got the request for the options:~p",[ReqParams]),
+  ACRequestHeaders = maps:get(<<"access-control-request-headers">>, Headers, <<"*">>),
+  RequestMethod = maps:get(<<"access-control-request-method">>, Headers),
+  lager:info("ACRequestHeaders:~p~n",[ACRequestHeaders]),
+  ResponseHeaders =  
+  	#{<<"content-type">>				=> <<"application/json;charset=utf-8">>,
+  	<<"Access-Control-Allow-Headers">>	=> ACRequestHeaders,
+  	<<"Access-Control-Allow-Methods">>	=> <<RequestMethod/binary, ",OPTIONS">>,
+  	<<"Access-Control-Max-Age">>		=> <<"1728000">>},
+  lager:info("ResponseHeaders for options:~p:",[ResponseHeaders]),
+  {204, [], ResponseHeaders}.
 
--- src/myaddressbook_client.erl
+++ src/myaddressbook_client.erl
@@ -3,48 +3,42 @@
 -behaviour(tivan_server).
 
 -export([start_link/0,
-
-		insert_address_api/4,
-		insert_address/1,
-		
+ 		insert_address_api/4,
 		view_address_api/4,
-		view_address/1]).
+		viewtag_address_api/4,
+		tag_address_api/4]).
 
 -export([init/1]).
 
-init([])	->
-	TableDef =
-		#{my_addresses	=>
-				#{columns	=>
-						#{name		=>
-								#{type	=>	binary
-                  ,unique=> true
-								,limit	=>	{3,30}}
-						,number	=>
-								#{type	=>	integer}}
-        }
-     },
+-define(ALLOWED_TAGS, [<<"FAVORITES">>, <<"BLOCKED">>]).
+
+init([]) ->
+  TableDef =
+    #{my_addresses => #{columns	=>
+                         #{name => #{type => binary,
+    								unique => true
+    								,limit => {3,30}}
+    					  ,number => #{type => integer}}
+                          ,tags => address_status
+    				   }
+    },
   {ok, TableDef}.
 
-start_link()	->
-	tivan_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+start_link() ->
+  tivan_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 insert_address_api(_Headers, ReqParams, _PathInfo, _State) ->
-	lager:info("Request : insert_address_api. Body :~p",[ReqParams]),
-	case catch insert_address(ReqParams) of
-    {X, Reason} when X == 'EXIT'; X == error  ->
-      lager:error("Request : insert_address_api. Reason : ~p", [Reason]),
-      {ok, [#{success => false}]};
+  lager:info("Request : insert_address_api. Body :~p",[ReqParams]),
+  case insert_address(ReqParams) of
+    {error, Reason} ->
+      {ok, [#{success => false, error => Reason}]};
     Result  ->
-	    {ok, [Result#{success => true}]}
+      {ok, [Result#{success => true}]}
   end.
 
 insert_address(Address) when is_map(Address) ->
-	Number = maps:get(number, Address),
-  case catch re:run(Number, "^[1-9][0-9]{9}$") of
-    {'EXIT',_} ->
-      lager:error("Invalid Number : ~p", [Number]),
-      {error, <<"Invalid Number">>};
+  Number = maps:get(number, Address),
+  case re:run(Number, "^[1-9][0-9]{9}$") of
     nomatch ->
       lager:error("Invalid Number : ~p", [Number]),
       {error, <<"Invalid Number">>};
@@ -54,25 +48,69 @@ insert_address(Address) when is_map(Address) ->
 
 view_address_api(_Headers, ReqParams, _PathInfo, _State) ->
   io:format("~p",[ReqParams]),
-	lager:info("Request : view_address_api. Body :~p",[ReqParams]),
-	case catch view_address(ReqParams) of
-    {X, Reason} when X == 'EXIT'; X == error  ->
-      lager:error("Error in view_address_api. Reason : ~p", [Reason]),
-      {ok, #{success => false}};
+  lager:info("Request : view_address_api. Body :~p",[ReqParams]),
+  case view_address(ReqParams) of
+    {error, Reason} -> 
+	  lager:error("Error in view_address. Reason : ~p", [Reason]),
+	  {ok, #{success => false, error => Reason}};
     [Result]  ->
-    	{ok, [Result#{success => true}]};
-    []        ->
-      {ok, [#{success => false, error => nodata}]}
+	  {ok, [Result#{success => true}]};
+    [] ->
+	  {ok, [#{success => false, error => nodata}]}
   end.
 
 view_address(Address) when is_map(Address) ->
-	tivan_server:get(?MODULE, my_addresses, Address).
+  tivan_server:get(?MODULE, my_addresses, Address).
 
-% {error, Reason} ->
-      % lager:error("Room exists failed ~p", [Reason]),
-      % Response = #{<<"status">> => <<"failure">>
-                   % ,<<"reason">> => Reason
-                  % },
-      % {ok, Response};
-% {ok, #{<<"status">> => <<"success">>,
-                  % <<"room_exists">> => Value}}
+viewtag_address_api(_Headers, ReqParams, _PathInfo, _State) ->
+  io:format("~p",[ReqParams]),
+  lager:info("Request : viewtag_address_api. Body :~p",[ReqParams]),
+  case view_tag(ReqParams) of
+    {error, Reason} ->
+      {ok, #{success => false, error => Reason}};
+    [] -> 
+	  {ok, [#{success => false, error => nodata}]};
+    Results ->
+      {ok, [begin
+              Name = maps:get(name, X),
+              #{success => true, name => Name}
+            end || X <- Results]}
+  end.
+
+view_tag(Tags) when is_map(Tags) ->
+  Tag = maps:get(tags, Tags, <<>>),
+  case tivan_server:get(?MODULE, my_addresses, #{match => #{tags => [Tag]}}) of
+    {error, Reason} -> {error, Reason};
+	Results -> filter_tag_objects(Results, string:uppercase(Tag), [])
+  end.
+
+filter_tag_objects([], _InputTag, Acc) ->
+  Acc;
+filter_tag_objects([Object | RestObject], InputTag, Acc) ->
+  Tag = maps:get(tags, Object, []),
+  case lists:member(InputTag, Tag) of
+    true -> filter_tag_objects(RestObject, InputTag, [Object | Acc]);
+    false -> filter_tag_objects(RestObject, InputTag, Acc)
+  end.
+  
+tag_address_api(_Headers, ReqParams, _PathInfo, _State) ->
+  io:format("~p",[ReqParams]),
+  lager:info("Request : tag_address_api. Body :~p",[ReqParams]),
+  Tag = maps:get(tag, ReqParams, []),
+  TagU = string:uppercase(Tag),
+  case view_address(maps:remove(tag, ReqParams)) of
+    {error, Reason} ->
+	  lager:error("Error in view_address. Reason : ~p", [Reason]),
+	  {ok, #{success => false, error => Reason}};
+    [Result]  ->
+	  case lists:member(TagU, ?ALLOWED_TAGS) of
+	    true ->
+		  tivan_server:remove(?MODULE, my_addresses, Result),
+		  tivan_server:put(?MODULE, my_addresses, Result#{tags => [TagU]}),
+	      {ok, [Result#{success => true, tags => [TagU]}]};
+		false ->
+	      {ok, [#{success => false, error => wrong_tag}]}
+	  end;
+    [] ->
+	      {ok, [#{success => false, error => nodata}]}
+  end.
